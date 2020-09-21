a06ca19ccd409a8a0042f66b6346c07394beb46f
Luke.Batchelor <batchelor.luke+git@gmail.com>
2020-09-21 18:53:11 +1000

    Adds argument parser

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,2 +1,18 @@
+import argparse
+
 def main():
-    print('here')
+    args = parse_args()
+    args.func()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    commands = parser.add_subparsers(dest="command")
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+def init():
+    print('Hello from init')
